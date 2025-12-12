c77712f7e9464c4e5bfa66668419f8029b2cfb8d
thebud4 <murilopmr13@hotmail.com>
2025-12-12 11:21:06 -0300

    refactor: clean up code formatting and improve component structure in Technology component

--- src/components/Tecnology.tsx
+++ src/components/Tecnology.tsx
@@ -1,21 +1,17 @@
 interface TechnologyProps {
-    icon: string;
-    altText: string;
+  icon: string;
+  altText: string;
 }
 
 const Technology: React.FC<TechnologyProps> = ({ icon, altText }) => {
-    return (
-        <div className="group size-16 relative flex items-center justify-center overflow-hidden rounded">
-            <img
-                src={icon}
-                alt={altText}
-                className="h-full w-full object-contain"
-            />
-            <span className="pointer-events-none absolute inset-0 flex items-center justify-center bg-darkGray text-background text-xs font-semibold opacity-0 transition-opacity duration-200 group-hover:opacity-100">
-                {altText}
-            </span>
-        </div>
-    );
+  return (
+    <div className="group size-16 relative flex items-center justify-center">
+      <img src={icon} alt={altText} className="h-full w-full object-contain" />
+      <span className="pointer-events-none absolute -bottom-8 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-darkGray px-2 py-1 text-xs font-semibold text-background opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+        {altText}
+      </span>
+    </div>
+  );
 };
 
 export default Technology;
